d9c0dd3d2e1972f0dde6cc0f965ba9a677f80868
sonali637 <sonalibiswal108@gmail.com>
2026-06-18 20:52:12 +0530

    while loop ex1

---
+++ javascript1/wloop_ex.js
@@ -0,0 +1,16 @@
+// while loop examples
+var x = 0;
+
+while (x < 5) {
+   console.log("x is currently: "+x);
+
+    if (x===3) {
+      console.log("x is equal to three");
+      break;
+    }
+
+   console.log("x is still less then 5, adding 1 to x");
+
+   x = x+1;
+
+}
