8c7d45e17bf6894cd7e795d98593a5064f8ea63e
sonali637 <sonalibiswal108@gmail.com>
2026-06-21 07:29:04 +0530

    function exercises

--- javascript1/func_ex1.js
+++ javascript1/func_ex1.js
@@ -1,3 +1,23 @@
 function hello(){
   console.log("hello world!");
 }
+
+// function examples
+// 1. multiplication
+function timesFive(numInput){
+  // local scope to the function
+   var result = numInput * 5
+   return result
+}
+
+// global scope
+var v ="GLOBAL V"
+var stuff = "GLOBAL STUFF"
+
+function fun(stuff) {
+   console.log(v);
+   stuff = "Reassign stuff inside func"
+   console.log(stuff);
+}
+fun()
+console.log(stuff);
