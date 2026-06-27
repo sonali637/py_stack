2eceba8ee663ad58c64f9f1a7b03fbc2a9506339
sonali637 <sonalibiswal108@gmail.com>
2026-06-27 22:06:00 +0530

    funct exer2

--- javascript1/func_exer2.js
+++ javascript1/func_exer2.js
@@ -1,1 +1,47 @@
 // problem number1
+function sleepIn(weekday, vacation) {
+    return (!weekday || vacation)
+}
+
+// 2nd problem - monkey trouble
+
+function monkeyTrouble(aSmile, bSmile) {
+    return (aSmile && bSmile) || (!aSmile && !bSmile)
+}
+
+// 3rd problem - strind times
+function stringTimes(str, n) {
+   var returnStr = "";
+   var i = 0;
+   while (i<n){
+     returnStr += str
+     i++
+   }
+   return returnStr
+}
+
+// 4th problem - lucky sum
+function luckySum(a, b, c) {
+   if (a === 13) {
+     return 0
+   }else if (b === 13) {
+     return a
+
+   }else if (c === 13) {
+     return a +b
+   }else {
+      return a+b+c
+   }
+}
+
+// 5th problem - you driving a little too fast, and a police officer stop monkeyTrouble
+function caught_speeding(speed, is_birtday) {
+    if (is_birthday)
+    speed -= 5
+}
+if (speed <= 60) {
+  return 0
+}
+if () {
+
+}
